e19205343bb6b6e799eb852980cefd63ccedcaf6
shreyayash <117577790+shreyayash@users.noreply.github.com>
2022-11-06 17:01:16 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var box
+function setup() {
+  createCanvas(400,400);
+  box= createSprite(200,200,30,30)
+}
+
+function draw() 
+{
+  background(30);
+  if(keyDown(RIGHT_ARROW)){
+    box.position.x+=5
+  }
+  if(keyDown(LEFT_ARROW)){
+    box.position.x-=5
+  }
+  if(keyDown(UP_ARROW)){
+    box.position.y-=5
+  }
+  if(keyDown(DOWN_ARROW)){
+    box.position.y+=5
+  }
+drawSprites()
+}
+
+
+
+
